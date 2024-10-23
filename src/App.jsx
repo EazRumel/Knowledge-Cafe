98449dc0fb945e396cad493ada8e6240019b16e6
Hemel <reaz.hemel@gmail.com>
2024-10-23 12:37:28 +0600

    the first initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold text-6xl bg-red-500'>Knowledge Cage</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
